59500e8837b1cc268c6c1899eb152cfd04e5a465
Muhammad <muhammadmh@ikhokha.com>
2022-06-12 18:21:01 +0200

    rum v5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import * as DataDogRUM from '@datadog/browser-rum'
+import { datadogRum } from '@datadog/browser-rum';
 import Navbar from './components/Navbar/Navbar';
 import Index from './pages/Index/Index';
 
 function App() {
+  const func = () => {
+    datadogRum.init({
+      applicationId: '6daecd2d-8edd-4222-8811-e5e6074e4821',
+      clientToken: 'pubafb549fcc3c4b3f9e2d859c8747448f9',
+      site: 'datadoghq.com',
+      service: 'accountant',
+
+      // Specify a version number to identify the deployed version of your application in Datadog
+      // version: '1.0.0',
+      sampleRate: 100,
+      premiumSampleRate: 100,
+      trackInteractions: true,
+      defaultPrivacyLevel: 'mask-user-input',
+    });
+    datadogRum.startSessionReplayRecording();
+  };
+
+  func();
+
   return (
     <div className="App">
       <div className="header">
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { datadogRum as DataDogRUM } from '@datadog/browser-rum';
 import App from './App';
+
+DataDogRUM.startSessionReplayRecording();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
