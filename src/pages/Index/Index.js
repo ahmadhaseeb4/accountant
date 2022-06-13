bfa6b4297873ced0fa725cd25b642f83e5ab7128
Muhammad <muhammadmh@ikhokha.com>
2022-06-13 13:43:49 +0200

    new url

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { datadogRum } from '@datadog/browser-rum';
 
 function Index() {
-  const link = 'https://ltwhtik5va.execute-api.us-east-1.amazonaws.com/accountant/accountant';
+  const link = 'https://i6vylz6l49.execute-api.us-east-1.amazonaws.com/scc/dcc';
   const getData = async () => {
     try {
       fetch(link).then((res) => {
