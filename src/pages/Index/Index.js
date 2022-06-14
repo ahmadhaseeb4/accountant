8f5d4ec64436344eaa55ba14b2d4585253cb006e
Muhammad <muhammadmh@ikhokha.com>
2022-06-14 16:47:25 +0200

    new strucutre

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,22 +1,7 @@
 import React from 'react';
-// import { datadogRum } from '@datadog/browser-rum';
 
 function Index() {
-  const link = 'https://i6vylz6l49.execute-api.us-east-1.amazonaws.com/scc/dcc';
-  const getData = async () => {
-    try {
-      fetch(link).then((res) => {
-        res.json().then((data) => {
-          if (data.errorType === 'Error') throw new Error(`Exceptonn: ${data.errorMessage}`);
-        });
-      });
-    } catch (err) {
-      console.log('Error: ', err);
-    }
-  };
-  return (
-    <button type="button" onClick={getData}>Get Data</button>
-  );
+  return (<div />);
 }
 
 export default Index;
